2ad2d079d09601ded6ce706941b59cb4307cf072
41815 <41815@172.19.146.167>
2017-05-16 18:10:22 +1000

    First version

--- src/java/uts/Customers.java
+++ src/java/uts/Customers.java
@@ -5,10 +5,49 @@
  */
 package uts;
 
+import java.io.Serializable;
+import javax.xml.bind.annotation.*;
+import java.util.ArrayList;
+
 /**
  *
- * @author 41815
+ * 
  */
-public class Customers {
-    
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlRootElement(name = "customers")
+public class Customers implements Serializable {
+
+    @XmlElement(name = "customer")
+    private ArrayList<Customer> list = new ArrayList<>();
+
+    public ArrayList<Customer> getList() {
+        return list;
+    }
+
+    public void addStudent(Customer student) {
+        list.add(student);
+    }
+
+    public void removeStudent(Customer student) {
+        list.remove(student);
+    }
+
+    public Customer getCustomer(String email) {
+        for (Customer customer : list) {
+            if (customer.getEmail().equals(email) ) {
+                return customer;
+            }
+        }
+        return null;
+    }
+
+    public Customer login(String email, String password) {
+        // For each student in the list...
+        for (Customer customer : list) {
+            if (customer.getEmail().equals(email)  & customer.getPassword().equals(password)) {
+                return customer; // Login correct. Return this student.
+            }
+        }
+        return null; // Login incorrect. Return null.
+    }
 }
